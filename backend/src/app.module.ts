f1cbed4f9eaef04c1d0da38be2d6d026eabd586a
SafaeQ <safaedani009@gmail.com>
2023-06-24 12:46:59 +0100

    ⚡️ add configuration database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,32 @@ import { UsersModule } from './users/users.module';
 import { ComplaintsModule } from './complaints/complaints.module';
 import { TeamsModule } from './teams/teams.module';
 import { EntitiesModule } from './entities/entities.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ComplaintsModule, TeamsModule, EntitiesModule],
+  imports: [
+    // loading environment variables
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    // initiating typeorm module according to the docs
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [__dirname + '/entities/*.js'],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    UsersModule,
+    ComplaintsModule,
+    TeamsModule,
+    EntitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
